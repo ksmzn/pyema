c33df00eca4978cc728f6a0c4608ac99f495921e
ksmzn <kossyfm@outlook.com>
2017-01-07 23:22:40 +0900

    argparseのメソッド変更

--- pyema/command_line.py
+++ pyema/command_line.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 
 def execute_ema(args, output_path=None, extracted_pathes=None, compless_method='rar'):
     target_files = args['fname']
+    ext = args['compressed_file_extention']
     for target_file in target_files:
         print('Processing {} file start ...'.format(basename(target_file)))
         ema = pyema.EMA(target_file, output_path, extracted_pathes)
@@ -17,8 +18,9 @@ def execute_ema(args, output_path=None, extracted_pathes=None, compless_method='
 def main(prog=None, args=None):
     parser = ArgumentParser(prog='pyema')
     parser.add_argument('fname', type=str, nargs='*', help='ema fname')
-    parser.add_argument('c', type=str, nargs='1', help='ema fname')
+    parser.add_argument('-e', '--compressed-file-extention', action='store_true', help='extention of compressed file', default=['zip'])
     args = vars(parser.parse_args())
+    print(args)
     execute_ema(args)
 # args = sys.argv
 # if len(args) < 2:
